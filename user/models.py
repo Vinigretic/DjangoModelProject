c9392bb8176fd0ff1bac2c8c3ed685ef9a8b87fd
Vinigretic <v.bakatina@gmail.com>
2022-07-04 18:22:49 +0300

    Create models - lesson_one_models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    city = models.CharField(max_length=20)
+
+class UserAdmin(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField()
+    is_admin = models.BooleanField()
