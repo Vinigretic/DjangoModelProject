d9d4375bf8076a780aa12503620b0cf7a0388adf
Vinigretic <v.bakatina@gmail.com>
2022-07-06 17:26:29 +0300

    Fields Type - lesson_two_fields_type

---
+++ cars/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Cars(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    age = models.DateTimeField()
+    color = models.CharField(max_length=50)
+    email_company = models.EmailField()
+
+
+class Dealer(models.Model):
+    brand = models.CharField(max_length=50)
+    email_dealer = models.EmailField()
+    image_dealer = models.FilePathField()
+    description = models.TextField()
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    rating = models.PositiveSmallIntegerField()
+
+
